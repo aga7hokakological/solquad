befecd4adc467392648d160eb46972a9e528216c
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2022-04-02 23:17:21 +0530

    wip: program structure change

--- programs/solqaud/src/lib.rs
+++ programs/solqaud/src/lib.rs
@@ -5,163 +5,250 @@ use spl_token::instruction::AuthorityType;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod solqaud {
+mod solquad {
     use super::*;
-
-    pub fn create_match_pool(ctx: Context<CreateMatchPool>, amount: u64, start: i64, end: i64) -> ProgramResult {
-        let match_pool = &mut ctx.accounts.match_pool;
-        // let creator = &mut ctx.accounts.creator;
-        let mut pool_id = 1;
-
-        // match_pool.creator = &mut ctx.accounts.creator;
-        match_pool.pool_id = pool_id;
-        match_pool.pool_amount = amount;
-        match_pool.start_time = start;
-        match_pool.end_time = end;
-
-        pool_id += 1;
- 
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 
-    pub fn add_projects_in_pool(ctx: Context<AddProjectsInPool>, p_name: String) -> ProgramResult {
-        let match_pool = &mut ctx.accounts.match_pool;
-        let project_owner = &mut ctx.accounts.project_owner;
-        let mut project_id = 1;
-        
-        let my_project = MyProject {
-            project_id: project_id,
-            project_key: *project_owner.to_account_info().key,
-            project_name: p_name.to_string(),
-            votes: 0,
-            amount_by_voters: 0,
-        };
+    const ESCROW_PDA_SEED: &[u8] = b"escrow";
 
-        match_pool.projects_in_pool.push(my_project);
-        project_id += 1;
-        
+    pub fn initialize_escrow(
+        ctx: Context<InitializeEscrow>,
+        initializer_amount: u64,
+        taker_amount: u64,
+    ) -> Result<()> {
+        ctx.accounts.escrow_account.initializer_key = *ctx.accounts.initializer.key;
+        ctx.accounts
+            .escrow_account
+            .initializer_deposit_token_account = *ctx
+            .accounts
+            .initializer_deposit_token_account
+            .to_account_info()
+            .key;
+        ctx.accounts
+            .escrow_account
+            .initializer_receive_token_account = *ctx
+            .accounts
+            .initializer_receive_token_account
+            .to_account_info()
+            .key;
+        ctx.accounts.escrow_account.initializer_amount = initializer_amount;
+        ctx.accounts.escrow_account.taker_amount = taker_amount;
+
+        let (pda, _bump_seed) = Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        token::set_authority(ctx.accounts.into(), AuthorityType::AccountOwner, Some(pda))?;
         Ok(())
     }
 
-    pub fn voting_started(ctx: Context<VotingStarted>, project_id: u8, amount: u64) -> ProgramResult {
-        let match_pool = &mut ctx.accounts.match_pool;
-        let voter = &mut ctx.accounts.voter;
+    pub fn cancel_escrow(ctx: Context<CancelEscrow>) -> Result<()> {
+        let (_pda, bump_seed) = Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        let seeds = &[&ESCROW_PDA_SEED[..], &[bump_seed]];
 
-        voter.voter = *voter.to_account_info().key;
-        let project_id = project_id as u8;
+        token::set_authority(
+            ctx.accounts
+                .into_set_authority_context()
+                .with_signer(&[&seeds[..]]),
+            AuthorityType::AccountOwner,
+            Some(ctx.accounts.escrow_account.initializer_key),
+        )?;
 
-        if i64::from(match_pool.start_time) > Clock::get().unwrap().unix_timestamp && i64::from(match_pool.end_time) < Clock::get().unwrap().unix_timestamp {
-            let project = &match_pool.projects_in_pool.iter_mut().find(|x| x.project_id == project_id);
-            match project {
-                Some(ref mut project) => {
-                    project.votes += 1;
-                    project.amount_by_voters += amount; 
-                },
-                None => {
-                    return Err(Errors::ProjectNotFoundError);
-                }
-            }
+        Ok(())
+    }
 
-            voter.project_voted_to.push(project.project_key);
+    pub fn exchange(ctx: Context<Exchange>) -> Result<()> {
+        // Transferring from initializer to taker
+        let (_pda, bump_seed) = Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        let seeds = &[&ESCROW_PDA_SEED[..], &[bump_seed]];
 
+        token::transfer(
+            ctx.accounts
+                .into_transfer_to_taker_context()
+                .with_signer(&[&seeds[..]]),
+            ctx.accounts.escrow_account.initializer_amount,
+        )?;
 
-            anchor_lang::solana_program::program::invoke(
-                &anchor_lang::solana_program::system_instruction::transfer(
-                    voter.to_account_info().key,
-                    project.to_account_info().key,
-                    amount,
-                ),
-                &[
-                    voter.to_account_info(),
-                    project.to_account_info(),
-                    ctx.accounts.system_program.to_account_info()
-                ],
-            )?;
-        }
+        token::transfer(
+            ctx.accounts.into_transfer_to_initializer_context(),
+            ctx.accounts.escrow_account.taker_amount,
+        )?;
+
+        token::set_authority(
+            ctx.accounts
+                .into_set_authority_context()
+                .with_signer(&[&seeds[..]]),
+            AuthorityType::AccountOwner,
+            Some(ctx.accounts.escrow_account.initializer_key),
+        )?;
 
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct CreateMatchPool<'info> {
+pub struct Initialize<'info> {}
+
+#[derive(Accounts)]
+#[instruction(initializer_amount: u64)]
+pub struct InitializeEscrow<'info> {
     #[account(mut)]
-    pub match_pool: Account<'info, MatchPool>,
-    pub creator: AccountInfo<'info>,
-    pub pool_account: Account<'info, TokenAccount>,
+    pub initializer: Signer<'info>,
+    #[account(
+        mut,
+        constraint = initializer_deposit_token_account.amount >= initializer_amount
+    )]
     pub initializer_deposit_token_account: Account<'info, TokenAccount>,
-    pub token_program: AccountInfo<'info>,
+    pub initializer_receive_token_account: Account<'info, TokenAccount>,
+    #[account(init, payer = initializer, space = 8 + EscrowAccount::LEN)]
+    pub escrow_account: Account<'info, EscrowAccount>,
     pub system_program: Program<'info, System>,
-}
-
-impl<'info> CreateMatchPool<'info> {
-    fn transfer_to_pool(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-        let cpi_accounts = Transfer {
-            from: self.initializer_deposit_token_account
-                    .to_account_info()
-                    .clone(),
-            to: self.pool_account
-                    .to_account_info()
-                    .clone(),
-            authority: self.creator.clone(),
-        };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
-    }
-
-    fn set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
-        let cpi_accounts = SetAuthority {
-            account_or_mint: self.pool_account.to_account_info().clone(),
-            current_authority: self.creator.clone(),
-        };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
-    }
+    pub token_program: Program<'info, Token>,
 }
 
 #[derive(Accounts)]
-pub struct VotingStarted<'info> {
+pub struct Exchange<'info> {
+    #[account(signer)]
+    pub taker: AccountInfo<'info>,
     #[account(mut)]
-    pub voter: Account<'info, Votes>,
-    pub match_pool: Account<'info, MatchPool>,
-    pub system_program: Program<'info, System>,
+    pub taker_deposit_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub taker_receive_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub pda_deposit_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub initializer_receive_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub initializer_main_account: AccountInfo<'info>,
+    #[account(
+        mut,
+        constraint = escrow_account.taker_amount <= taker_deposit_token_account.amount,
+        constraint = escrow_account.initializer_deposit_token_account == *pda_deposit_token_account.to_account_info().key,
+        constraint = escrow_account.initializer_receive_token_account == *initializer_receive_token_account.to_account_info().key,
+        constraint = escrow_account.initializer_key == *initializer_main_account.key,
+        close = initializer_main_account
+    )]
+    pub escrow_account: Account<'info, EscrowAccount>,
+    pub pda_account: AccountInfo<'info>,
+    pub token_program: Program<'info, Token>,
 }
 
-#[derive(Accounts)] 
-pub struct AddProjectsInPool<'info> {
+#[derive(Accounts)]
+pub struct CancelEscrow<'info> {
+    pub initializer: AccountInfo<'info>,
     #[account(mut)]
-    pub match_pool: Account<'info, MatchPool>,
-    pub project_owner: Account<'info, MyProject>,
+    pub pda_deposit_token_account: Account<'info, TokenAccount>,
+    pub pda_account: AccountInfo<'info>,
+    #[account(
+        mut,
+        constraint = escrow_account.initializer_key == *initializer.key,
+        constraint = escrow_account.initializer_deposit_token_account == *pda_deposit_token_account.to_account_info().key,
+        close = initializer
+    )]
+    pub escrow_account: Account<'info, EscrowAccount>,
+    pub token_program: Program<'info, Token>,
 }
 
-// #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
 #[account]
-pub struct MyProject {
-    pub project_id: u8,
-    pub project_key: Pubkey,
+pub struct Project<'info> {
+    pub project_id: u32,
+    pub project_owner: Pubkey,
     pub project_name: String,
-    pub votes: u64,
-    pub amount_by_voters: u64,
+    pub votes_count: u64,
 }
 
 #[account]
-pub struct MatchPool {
-    pool_id: u8,
-    pool_owner: Pubkey,
-    pool_amount: u64,
-    start_time: i64,
-    end_time: i64,
-    projects_in_pool: Vec<MyProject>,
+pub struct Pool<'info> {
+    pub pool_id: u32,
+    pub pool_owner: Pubkey,
+    pub pool_token: Pubkey,
+    pub pool_amount: u64,
+    pub projects: Vec<Project>,
 }
 
 #[account]
-pub struct Votes {
+pub struct Voter<'info> {
     pub voter: Pubkey,
-    pub project_voted_to: Vec<Pubkey>,
+    pub voted_to: Pubkey,
 }
 
-#[error]
-pub enum Errors {
-    #[msg("Pool creation error")]
-    PoolCreationError,
-    #[msg("Project not found error")]
-    ProjectNotFoundError,
+#[account]
+pub struct EscrowAccount<'info> {
+    pub initializer_key: Pubkey,
+    pub initializer_deposit_token_account: Pubkey,
+    pub initializer_receive_token_account: Pubkey,
+    pub initializer_amount: u64,
+    pub taker_amount: u64,
+}
+
+impl EscrowAccount {
+    pub const LEN: usize = 32 + 32 + 32 + 8 + 8;
+}
+
+impl<'info> From<&mut InitializeEscrow<'info>> 
+    for CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        fn from(accounts: &mut InitializeEscrow<'info>) -> Self {
+            let cpi_accounts = SetAuthority {
+                account_or_mint: accounts
+                    .initializer_deposit_token_account
+                    .to_account_info()
+                    .clone(),
+                current_authority: accounts.initializer.to_account_info().clone(),
+            };
+
+            let cpi_program = accounts.token_program.to_account_info();
+            CpiContext::new(cpi_program, cpi_accounts)
+        }
+    }
+
+impl<'info> CancelEscrow<'info> {
+    fn into_set_authority_context(&self) -> 
+        CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+            let cpi_accounts = SetAuthority {
+                account_or_mint: self.pda_deposit_token_account.to_account_info().clone(),
+                current_authority: self.pda_account.clone(),
+            };
+
+            let cpi_program = self.token_program.to_account_info();
+            CpiContext::new(cpi_program, cpi_accounts)
+        }
+}
+
+impl<'info> Exchange<'info> {
+    fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        let cpi_accounts = SetAuthority {
+            account_or_mint: self.pda_deposit_token_account.to_account_info().clone(),
+            current_authority: self.pda_account.clone(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+impl<'info> Exchange<'info> {
+    fn into_transfer_to_taker_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.pda_deposit_token_account.to_account_info().clone(),
+            to: self.taker_receive_token_account.to_account_info().clone(),
+            authority: self.pda_account.clone(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+impl<'info> Exchange<'info> {
+    fn into_transfer_to_initializer_context(
+        &self,
+    ) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.taker_deposit_token_account.to_account_info().clone(),
+            to: self
+                .initializer_receive_token_account
+                .to_account_info()
+                .clone(),
+            authority: self.taker.clone(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
 }
